97c323af88e935f964d71de0993c908db5db9584
GTS-Intern-Santosh <gtsintern1@gmail.com>
2025-03-06 13:48:55 +1100

    UserId is also stored in the product table when user creates a product

--- server/models/Product.js
+++ server/models/Product.js
@@ -16,6 +16,14 @@ const Product = db.define(
       type: DataTypes.FLOAT,
       allowNull: false,
     },
+    UserId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Users',
+        key: 'id',
+      },
+    },
   },
   {
     timestamps: true,
